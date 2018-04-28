fdc3ead2c6d3dd83fc44aa86e66bf14629888797
kamilSzelc <kamil.szelc@tlen.pl>
2018-04-28 10:07:52 +0200

    "Message"

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -7,24 +7,27 @@ public class Menu {
     }
 
     private static void MenuKsiegarni() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Co chcesz zrobić?");
-        System.out.println("1. Wyjdź z menu");
-        System.out.println("2. Wyświetl kontakt");
+        for (int i = 0; i < 1;) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Co chcesz zrobić?");
+            System.out.println("1. Wyjdź z menu");
+            System.out.println("2. Wyświetl kontakt");
 
-        int i = scanner.nextInt();
-        switch (i){
-            case 1:
-                System.out.println("Wyszedłeś z menu");
-                break;
-            case 2:
-                System.out.println("Kontakt z księgarnią: ");
-                System.out.println("Numer telefonu: 111 222 333");
-                System.out.println("E-mail: ksiegarnia@ksiazki.com");
-                break;
-            default:
-                System.out.println("Nie istnieje taka komenda");
-                break;
-        }
+            int a = scanner.nextInt();
+            switch (a){
+                case 1:
+                    System.out.println("Wyszedłeś z menu");
+                    i++;
+                    return;
+                case 2:
+                    System.out.println("Kontakt z księgarnią: ");
+                    System.out.println("Numer telefonu: 111 222 333");
+                    System.out.println("E-mail: ksiegarnia@ksiazki.com");
+                    break;
+                default:
+                    System.out.println("Nie istnieje taka komenda, wybierz ponownie");
+                    break;
+            }
+        } return;
     }
 }
